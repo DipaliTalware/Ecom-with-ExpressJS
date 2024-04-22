36652d8460b83045dc2a7f708a3fdc100c8a4066
DipaliTalware <d.talware@gmail.com>
2024-04-22 18:49:09 +0200

    2 APIs created for each product and bestseller section

--- data/data.js
+++ data/data.js
@@ -7,6 +7,33 @@ const products = [
 			'The Dots Necklace is a tribute to your unique path, with each dot representing a milestone, an experience, a decision, a day. Diameter of the dots: approx. 1 mm Length adjustable between 37 and 41 cm',
 		img: 'http://images.ctfassets.net/r083ag4j14da/4N7gXxsTwLy8Wvkm2Pfxsd/600045e6008d2883ebb888ab5d8a9b1e/01_900x.webp',
 	},
+
+	{
+		id: '2',
+		title: 'Line Ear Climber',
+		price: '41 EUR',
+		description:
+			'The Line Ear Climber looks both edgy and playful and underlines every look. The handmade eyecatcher can be perfectly combined with cool ear cuffs.',
+		img: 'https://images.ctfassets.net/r083ag4j14da/4trICR4qEJ41mnyo6SYNY1/a778d0c95c7e7d2224a429fa26a35f3a/main_900x.webp',
+	},
+
+	{
+		id: '3',
+		title: 'Line Hoops Small',
+		price: '43 EUR',
+		description:
+			'Our customers love the classic Line Hoops because they complete every outfit and every occasion. Handcrafted, waterproof and minimalist - the perfect earring to enhance your look without overdoing it.',
+		img: 'https://images.ctfassets.net/r083ag4j14da/2LhJ5BuKWXovyHEAMeY3lL/f1bb6372aab3ece32a7ce1444445c8a9/main_900x.webp',
+	},
+
+	{
+		id: '4',
+		title: 'Double Ring',
+		price: '49 EUR',
+		description:
+			'Our customers love the Double Ring because it is comfortable to wear and brings twice as much joy as other rings. It creates a harmonious, cool look that stands out',
+		img: 'https://images.ctfassets.net/r083ag4j14da/1G9WCNhLQKIdTc64CU6boF/6ef6081840c6a4dfdf5627eb5b9ddf7e/ringMain_900x.webp',
+	},
 ];
 
 export default products;
--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
 	res.json(data);
+	console.log(data);
+});
+
+app.get('/:id', (req, res) => {
+	const { id } = req.params;
+	const product = data.find((product) => id === product.id);
+	res.json(product);
+	console.log(data);
 });
 
 const server = app.listen(port, () => console.log('Hello'));
